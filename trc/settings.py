cf6f370a716ea4b2d657b84ffc6c094c35df0cce
nautilo <8lapse@gmail.com>
2022-08-15 23:55:07 -0400

    directorio plantillas

--- trc/settings.py
+++ trc/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'trc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/Program Files/trc_ventas/trc/trc/plantillas'],
+        'DIRS': ['/home/nautilo/trc/trc/plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
